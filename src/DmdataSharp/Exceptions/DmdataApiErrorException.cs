26f5483b85b8eb9dfb191bc0ffd12274cf798219
ingen084 <ingen188@gmail.com>
2022-03-06 13:50:42 +0900

    例外追加

--- src/DmdataSharp/Exceptions/DmdataApiErrorException.cs
+++ src/DmdataSharp/Exceptions/DmdataApiErrorException.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DmdataSharp.ApiResponses;
+using System;
 
 namespace DmdataSharp.Exceptions
 {
@@ -7,5 +8,36 @@ namespace DmdataSharp.Exceptions
 	/// </summary>
 	public class DmdataApiErrorException : Exception
 	{
+		/// <summary>
+		/// リクエストID
+		/// </summary>
+		public string ResponseId { get; set; }
+
+		/// <summary>
+		/// レスポンス時刻
+		/// </summary>
+		public DateTime ResponseTime { get; set; }
+
+		/// <summary>
+		/// エラーコード
+		/// </summary>
+		public int? ErrorCode { get; set; }
+
+		/// <summary>
+		/// エラーメッセージ
+		/// </summary>
+		public string? ErrorMessage { get; set; }
+
+		/// <summary>
+		/// 例外を初期化する
+		/// </summary>
+		/// <param name="response">元にするレスポンス</param>
+		public DmdataApiErrorException(DmdataResponse response)
+		{
+			ResponseId = response.ResponseId;
+			ResponseTime = response.ResponseTime;
+			ErrorCode = response.Error?.Code;
+			ErrorMessage = response.Error?.Message;
+		}
 	}
 }
